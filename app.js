f615868f00bced709b4267f05fb865796a49b6ec
Kunli Zhang <kunlizhang7@gmail.com>
2022-11-16 15:11:49 -0500

    login and signup working

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ var express = require('express');
 var routes = require('./routes/routes.js');
 var session = require('express-session');
 var app = express()
+app.use(session({secret: "mySecret"})); //Not sure if I am using sessions properly here?
 app.use(express.urlencoded());
 
 /**
  * Routes for user
  */
 app.get('/', routes.get_main);
-app.get('/checklogin', routes.check_login);
+app.post('/checklogin', routes.check_login);
 app.get('/signup', routes.signup);
+app.post('/createaccount', routes.create_account);
+
+/**
+ * Routes for wall
+ */
+app.get('/wall', routes.get_wall);
 
 app.listen(8080);
 console.log('Server running on port 8080.')
--- routes/routes.js
+++ routes/routes.js
@@ -13,20 +13,19 @@ var getMain = function(req, res) {
 
 // Checks login is valid
 var checkLogin = function(req, res) {
-    var login = req.body.username;
+    var login = req.body.login;
     var password = req.body.password;
 
-    db.verify_user(username, password, function(err, data) {
+    db.verify_user(login, password, function(err, data) {
         if (err) {
             console.log(err);
-            res.redirect('/?error=' + encodeuricomponent("invalid_login"));
+            res.redirect('/?error=' + encodeURIComponent("invalid_login"));
         } else {
             if (data) {
                 console.log("User " + login + " logged in.");
-                // TODO implement homepage here
-                res.redirect('/')
+                res.redirect('/wall')
             } else {
-                res.redirect('/?error=' + encodeuricomponent("invalid_login"));
+                res.redirect('/?error=' + encodeURIComponent("invalid_login"));
             } 
         }
     })
@@ -40,7 +39,35 @@ var signup = function(req, res) {
 
 // Create an account
 var createAccount = function(req, res) {
+    var login = req.body.login;
+    var password = req.body.password;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+    var affiliation = req.body.affiliation;
+    var birthday = req.body.birthday;
+    
+    db.exists_user(login, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else if (data) {
+            res.redirect('/signup?error=' + encodeURIComponent('invalid_username'));
+        } else {
+            db.add_user(login, password, firstname, lastname, email, affiliation, birthday, ['default'], function() {
+                req.session.login = login;
+                console.log('User ' + login + ' logged in.');
+                res.redirect('/wall'); 
+            });
+        }
+    });
+}
 
+/**
+ * Routes for wall
+ */
+
+var getWall = function(req, res) {
+    res.render('wall.ejs');
 }
 
 var routes = {
@@ -48,6 +75,7 @@ var routes = {
     check_login: checkLogin,
     signup: signup,
     create_account: createAccount,
+    get_wall: getWall,
 };
 
 module.exports = routes;
