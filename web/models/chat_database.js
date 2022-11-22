2c4aa5bf3ac5c455c49347fe685309120f80003b
Alberto Gaytan <agaytan@sas.upenn.edu>
2022-11-22 00:11:22 +0000

    basic chat

--- web/models/chat_database.js
+++ web/models/chat_database.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region:'us-east-1'});
 var db = new AWS.DynamoDB();
-const {v4: uuidv4} = require('uuid')
+const {v4: uuidv4} = require('uuid');
 
 var myDB_create_chat = function(user_1, user_2, callback) {
 	console.log("Creating chat between " + user_1 + " and " + user_2);
@@ -27,24 +27,6 @@ var myDB_create_chat = function(user_1, user_2, callback) {
 							}
 				       }
 					}
-				},
-				{
-					PutRequest: {
-						Item: {
-					       "user_1": {
-					          S: user_2
-					        },
-					        "user_2": {
-							  S: user_1
-							},
-					        "chat_id": { 
-					          S: id
-					        },
-					        "group_chat": {
-							  S: "false" // group chat status initially false
-							}
-				       }
-					}
 				}
 			]
 		}
@@ -55,7 +37,43 @@ var myDB_create_chat = function(user_1, user_2, callback) {
 		if (err) {
 			callback(err);
 		} else {
-			callback(null, id);
+			var params2 = {
+				RequestItems: {
+					"user_in_chat": [
+						{
+							PutRequest: {
+								Item: {
+							       "login": {
+							          S: user_1
+							        },
+							        "chat_id": {
+										S: id
+									}
+								}
+						      }
+							},
+						{
+							PutRequest: {
+								Item: {
+							       "login": {
+							          S: user_2
+							        },
+							        "chat_id": {
+										S: id
+									}
+						       }
+							}
+						}
+					]
+				}
+			};
+			db.batchWriteItem(params2, function(err2, data2) {
+				if (err2) {
+					callback(err2);
+				} else {
+					callback(null, id);
+				}
+			});
 		}
 	});
 }
@@ -99,7 +117,7 @@ var myDB_get_messages = function(chat_id, callback) {
           AttributeValueList: [ {S: chat_id} ]
         },
       },
-      TableName: "chats",
+      TableName: "messages",
   };
 
   db.query(params, function(err, data) {
@@ -142,11 +160,57 @@ var myDB_add_message = function(chat_id, content, time, user, callback) {
   });
 }
 
+var myDB_add_user_chat = function(login, chat_id, callback) {
+  console.log('Adding to: ' + login); 
+  
+  var params = {
+      Item: {
+       "login": {
+          S: login
+        },
+        "chat_id": { 
+          S: chat_id
+        }
+      },
+      TableName: 'user_in_chat',
+      ReturnValues: 'NONE'
+  };
+
+  db.putItem(params, function(err, data){
+    if (err)
+      callback(err);
+    else
+      callback(null, 'Success');
+  });
+}
+
+var myDB_get_user_chats = function(login, callback) {
+	var params = {
+      KeyConditions: {
+        login: {
+          ComparisonOperator: 'EQ',
+          AttributeValueList: [ {S: login} ]
+        },
+      },
+      TableName: "user_in_chat",
+  };
+
+  db.query(params, function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+	  callback(err, data);
+    }
+  });
+}
+
 var database = { 
   createChat: myDB_create_chat,
   checkChat: myDB_check_chat,
   getMessages: myDB_get_messages,
-  addMessage: myDB_add_message
+  addMessage: myDB_add_message,
+  addUserChat: myDB_add_user_chat,
+  getUserChats: myDB_get_user_chats
 };
 
 module.exports = database;
